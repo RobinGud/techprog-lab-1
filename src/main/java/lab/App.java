b9a413c551caa474b9863d7dc2c77b1790ed36b8
Nikita Matsnev <niki-sim@mail.ru>
2020-02-15 21:11:00 +0300

    done second task

--- src/main/java/lab/App.java
+++ src/main/java/lab/App.java
@@ -10,6 +10,25 @@ package lab;
 */
 public class App {
     public static void main(String[] args){
+        // Task 1
         System.out.println("Starting project");
+        // Task 2
+        byte v_byte = 120;
+        short v_short = 129;
+        char v_char = 'a'; // add ''
+        int v_int = 65999;
+        long v_long = Long.parseUnsignedLong("4294967296"); // var is overflow
+        float v_float = 0.33333334f; // add f 
+        double v_double = 0.33333333333;
+        boolean v_boolean = true; // change var name
+
+        System.out.println("v_byte: " + v_byte);
+        System.out.println("v_short: " + v_short);
+        System.out.println("v_char: " + v_char);
+        System.out.println("v_int: " + v_int);
+        System.out.println("v_long: " + v_long);
+        System.out.println("v_float: " + v_float);
+        System.out.println("v_double: " + v_double);
+        System.out.println("v_boolean: " + v_boolean);
     }
 }
